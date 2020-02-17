8584e42a7b8d53072f7f967d8ad599192dc56b32
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-17 22:06:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,8 +31,8 @@ monopoly = {
 	  :rent_in_dollars => 
 	  {
 	    :one_piece_owned => 25,
-	    :two_pieces_owned => 50
-	    :three_pieces_owned => 100
+	    :two_pieces_owned => 50,
+	    :three_pieces_owned => 100,
 	    :four_pieces_owned => 200
 	  }
 	 }, 
