f28dd9709096aff0dfe825aa2ecaba41aca601eb
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-17 22:08:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,8 +31,9 @@ monopoly = {
 	 :pieces => 4, 
 	 :names => {
 	    :reading_railroad => {}
-	    :pennsylvania_railroad=>
-	    :b_and_o_railroad=>
+	    :pennsylvania_railroad => {}
+	    :b_and_o_railroad => {}
+	    :shortline => {}
 	  }, 
 	  :rent_in_dollars => 
 	  {
